6af3ef13d175070bf881cc4bb4808e002f91c919
Jonathan Vieira <wallacejsv@gmail.com>
2020-02-18 23:42:52 -0300

    rotas

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Routes from './routes';
+
 import Header from './components/Header';
 import Main from './pages/main';
 
@@ -7,7 +9,7 @@ import './styles.css';
 const App = () => (
 	<div className="App">
 		<Header />
-		<Main />
+		<Routes />
 	</div>
 );
 
